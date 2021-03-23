7ad6091777af3d9b6ecdd93babc0d17c1904968f
Na21k <hulevych@hotmail.com>
2021-03-23 12:51:21 +0200

    added interface and class files for authorization service

---
+++ NotesAndReminders/NotesAndReminders.Android/Services/FirebaseAuthService.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using NotesAndReminders.Droid.Services;
+using NotesAndReminders.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FirebaseAuthService))]
+namespace NotesAndReminders.Droid.Services
+{
+	public class FirebaseAuthService : IAuthorizationService
+	{
+
+	}
+}
---
+++ NotesAndReminders/NotesAndReminders/Services/IAuthorizationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesAndReminders.Services
+{
+	public interface IAuthorizationService
+	{
+
+	}
+}

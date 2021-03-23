a97a8278c451261674a3ef3e75536b42b902403b
Na21k <hulevych@hotmail.com>
2021-03-23 19:40:26 +0200

    implemented IAuthorizationService

--- NotesAndReminders/NotesAndReminders.Android/Services/FirebaseAuthService.cs
+++ NotesAndReminders/NotesAndReminders.Android/Services/FirebaseAuthService.cs
@@ -1,16 +1,10 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Gms.Extensions;
+using Firebase.Auth;
 using NotesAndReminders.Droid.Services;
+using NotesAndReminders.Exceptions;
 using NotesAndReminders.Models;
 using NotesAndReminders.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 [assembly: Xamarin.Forms.Dependency(typeof(FirebaseAuthService))]
@@ -18,16 +12,66 @@ namespace NotesAndReminders.Droid.Services
 {
 	public class FirebaseAuthService : IAuthorizationService
 	{
-		public bool IsLoggedIn => throw new NotImplementedException();
+		private FirebaseAuth _auth = FirebaseAuth.Instance;
 
-		public Task<(User, string)> LogIn(string email, string password)
+		public bool IsLoggedIn
 		{
-			throw new NotImplementedException();
+			get
+			{
+				var user = _auth.CurrentUser;
+				return user != null;
+			}
+		}
+
+		public async Task<(User, string)> LogIn(string email, string password)
+		{
+			try
+			{
+				var user = await _auth.SignInWithEmailAndPasswordAsync(email, password);
+				var token = await user.User.GetIdToken(false) as GetTokenResult;
+
+				var userModel = new User()
+				{
+					Email = user.User.Email,
+					UserName = user.User.DisplayName,
+					UserId = _auth.CurrentUser.Uid
+				};
+
+				if (token == null)
+				{
+					throw new NullReferenceException($"{typeof(FirebaseAuthService)}: {nameof(token)} was null");
+				}
+
+				return (userModel, token?.Token);
+			}
+			catch (FirebaseAuthInvalidUserException ex)
+			{
+				ex.PrintStackTrace();
+				throw new Exception(ex.Message);
+			}
+			catch (FirebaseAuthInvalidCredentialsException ex)
+			{
+				ex.PrintStackTrace();
+				throw new InvalidCredentialsException(ex.Message, ex.StackTrace);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.StackTrace);
+				throw new Exception(ex.Message);
+			}
 		}
 
 		public bool LogOut()
 		{
-			throw new NotImplementedException();
+			try
+			{
+				_auth.SignOut();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
 		}
 	}
 }
---
+++ NotesAndReminders/NotesAndReminders/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NotesAndReminders.Exceptions
+{
+	public class InvalidCredentialsException : Exception
+	{
+		public new string Message { get; private set; }
+		public new string StackTrace { get; private set; }
+
+		public InvalidCredentialsException(string message, string stacktrace)
+		{
+			Message = message;
+			StackTrace = stacktrace;
+		}
+	}
+}

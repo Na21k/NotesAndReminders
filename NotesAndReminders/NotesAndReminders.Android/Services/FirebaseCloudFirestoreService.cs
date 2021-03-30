8a1e9699b91ba8c8231ffa486976f38b90d5deeb
Na21k <hulevych@hotmail.com>
2021-03-30 21:15:29 +0300

    added db interface and (empty) service

---
+++ NotesAndReminders/NotesAndReminders.Android/Services/FirebaseCloudFirestoreService.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using NotesAndReminders.Droid.Services;
+using NotesAndReminders.Models;
+using NotesAndReminders.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FirebaseCloudFirestoreService))]
+namespace NotesAndReminders.Droid.Services
+{
+	public class FirebaseCloudFirestoreService : IDBService
+	{
+		public Task<bool> AddNoteAsync(Note note)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> AddNoteTypeAsync(NoteType noteType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteNoteAsync(Note note)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteNoteTypeAsync(NoteType noteType)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Note>> GetAllNotesAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<NoteType>> GetAllNoteTypesAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Note> GetNoteAsync(string noteId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<NoteType> GetNoteTypeAsync(string noteTypeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateNoteAsync(Note note)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> UpdateNoteTypeAsync(NoteType noteType)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ NotesAndReminders/NotesAndReminders/Services/IDBService.cs
@@ -0,0 +1,20 @@
+﻿using NotesAndReminders.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NotesAndReminders.Services
+{
+	public interface IDBService
+	{
+		Task<bool> AddNoteAsync(Note note);
+		Task<bool> DeleteNoteAsync(Note note);
+		Task<bool> UpdateNoteAsync(Note note);
+		Task<bool> AddNoteTypeAsync(NoteType noteType);
+		Task<bool> DeleteNoteTypeAsync(NoteType noteType);
+		Task<bool> UpdateNoteTypeAsync(NoteType noteType);
+		Task<Note> GetNoteAsync(string noteId);
+		Task<List<Note>> GetAllNotesAsync();
+		Task<NoteType> GetNoteTypeAsync(string noteTypeId);
+		Task<List<NoteType>> GetAllNoteTypesAsync();
+	}
+}

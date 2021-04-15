dab4de67fde881132c8672d74519ed68cf99df1f
Na21k <hulevych@hotmail.com>
2021-04-15 23:14:26 +0300

    added a converter for Color

---
+++ NotesAndReminders/NotesAndReminders/Converters/ColorJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using Xamarin.Forms;
+
+namespace NotesAndReminders.Converters
+{
+	public class ColorJsonConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == Color.Default.GetType();
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var color = Color.FromHex(existingValue as string);
+
+			return color;
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			if (value is Color color)
+			{
+				serializer.Serialize(writer, color.ToHex());
+			}
+		}
+	}
+}
--- NotesAndReminders/NotesAndReminders/Models/NoteType.cs
+++ NotesAndReminders/NotesAndReminders/Models/NoteType.cs
@@ -1,10 +1,13 @@
 ﻿using Xamarin.Forms;
+using Newtonsoft.Json;
+using NotesAndReminders.Converters;
 
 namespace NotesAndReminders.Models
 {
 	public class NoteType : Identifiable, IDBItem
 	{
 		public string Name { get; set; }
+		[JsonConverter(typeof(ColorJsonConverter))]
 		public Color Color { get; set; }
 	}
 }

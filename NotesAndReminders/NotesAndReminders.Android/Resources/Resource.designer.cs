9e9cda3fa81fae5b3265dc38c9be3f190376e244
Na21k <hulevych@hotmail.com>
2021-05-20 23:16:33 +0300

    added new icons

--- NotesAndReminders/NotesAndReminders.Android/Resources/Resource.designer.cs
+++ NotesAndReminders/NotesAndReminders.Android/Resources/Resource.designer.cs
@@ -9087,71 +9087,89 @@ namespace NotesAndReminders.Droid
 			// aapt resource value: 0x7F07008D
 			public const int icn_archive_menu_light = 2131165325;
 			
-			// aapt resource value: 0x7F07008E
-			public const int icn_categories_dark = 2131165326;
-			
-			// aapt resource value: 0x7F07008F
-			public const int icn_categories_light = 2131165327;
-			
 			// aapt resource value: 0x7F070090
-			public const int icn_emptyArchive = 2131165328;
+			public const int icn_bellDelete_dark = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int icn_image_dark = 2131165329;
+			public const int icn_bellDelete_light = 2131165329;
+			
+			// aapt resource value: 0x7F07008E
+			public const int icn_bell_dark = 2131165326;
+			
+			// aapt resource value: 0x7F07008F
+			public const int icn_bell_light = 2131165327;
 			
 			// aapt resource value: 0x7F070092
-			public const int icn_image_light = 2131165330;
+			public const int icn_categories_dark = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int icn_itsEmptyHere = 2131165331;
+			public const int icn_categories_light = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int icn_newNote_dark = 2131165332;
+			public const int icn_emptyArchive = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int icn_newNote_light = 2131165333;
+			public const int icn_image_dark = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int icn_note_dark = 2131165334;
+			public const int icn_image_light = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int icn_note_light = 2131165335;
+			public const int icn_itsEmptyHere = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int icn_profile_dark = 2131165336;
+			public const int icn_newNote_dark = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int icn_profile_light = 2131165337;
+			public const int icn_newNote_light = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int icn_save_dark = 2131165338;
+			public const int icn_note_dark = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int icn_save_light = 2131165339;
+			public const int icn_note_light = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int icn_search_dark = 2131165340;
+			public const int icn_profile_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int icn_search_light = 2131165341;
+			public const int icn_profile_light = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int icn_trash_dark = 2131165342;
+			public const int icn_save_dark = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int icn_trash_light = 2131165343;
+			public const int icn_save_light = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int icn_unarchive_dark = 2131165344;
+			public const int icn_search_dark = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int icn_unarchive_light = 2131165345;
+			public const int icn_search_light = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int icon_about = 2131165346;
+			public const int icn_settings_dark = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int icon_feed = 2131165347;
+			public const int icn_settings_light = 2131165347;
+			
+			// aapt resource value: 0x7F0700A4
+			public const int icn_trash_dark = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int icn_trash_light = 2131165349;
+			
+			// aapt resource value: 0x7F0700A6
+			public const int icn_unarchive_dark = 2131165350;
+			
+			// aapt resource value: 0x7F0700A7
+			public const int icn_unarchive_light = 2131165351;
+			
+			// aapt resource value: 0x7F0700A8
+			public const int icon_about = 2131165352;
+			
+			// aapt resource value: 0x7F0700A9
+			public const int icon_feed = 2131165353;
 			
 			// aapt resource value: 0x7F070081
 			public const int ic_mtrl_chip_checked_black = 2131165313;
@@ -9162,65 +9180,65 @@ namespace NotesAndReminders.Droid
 			// aapt resource value: 0x7F070083
 			public const int ic_mtrl_chip_close_circle = 2131165315;
 			
-			// aapt resource value: 0x7F0700A4
-			public const int logo = 2131165348;
-			
-			// aapt resource value: 0x7F0700A5
-			public const int logo_large = 2131165349;
-			
-			// aapt resource value: 0x7F0700A6
-			public const int mtrl_snackbar_background = 2131165350;
-			
-			// aapt resource value: 0x7F0700A7
-			public const int mtrl_tabs_default_indicator = 2131165351;
-			
-			// aapt resource value: 0x7F0700A8
-			public const int navigation_empty_icon = 2131165352;
-			
-			// aapt resource value: 0x7F0700A9
-			public const int notification_action_background = 2131165353;
-			
 			// aapt resource value: 0x7F0700AA
-			public const int notification_bg = 2131165354;
+			public const int logo = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int notification_bg_low = 2131165355;
+			public const int logo_large = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int notification_bg_low_normal = 2131165356;
+			public const int mtrl_snackbar_background = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int notification_bg_low_pressed = 2131165357;
+			public const int mtrl_tabs_default_indicator = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int notification_bg_normal = 2131165358;
+			public const int navigation_empty_icon = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int notification_bg_normal_pressed = 2131165359;
+			public const int notification_action_background = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int notification_icon_background = 2131165360;
+			public const int notification_bg = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int notification_template_icon_bg = 2131165361;
+			public const int notification_bg_low = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int notification_template_icon_low_bg = 2131165362;
+			public const int notification_bg_low_normal = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int notification_tile_bg = 2131165363;
+			public const int notification_bg_low_pressed = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int notify_panel_notification_icon_bg = 2131165364;
+			public const int notification_bg_normal = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int tooltip_frame_dark = 2131165365;
+			public const int notification_bg_normal_pressed = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int tooltip_frame_light = 2131165366;
+			public const int notification_icon_background = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int xamarin_logo = 2131165367;
+			public const int notification_template_icon_bg = 2131165367;
+			
+			// aapt resource value: 0x7F0700B8
+			public const int notification_template_icon_low_bg = 2131165368;
+			
+			// aapt resource value: 0x7F0700B9
+			public const int notification_tile_bg = 2131165369;
+			
+			// aapt resource value: 0x7F0700BA
+			public const int notify_panel_notification_icon_bg = 2131165370;
+			
+			// aapt resource value: 0x7F0700BB
+			public const int tooltip_frame_dark = 2131165371;
+			
+			// aapt resource value: 0x7F0700BC
+			public const int tooltip_frame_light = 2131165372;
+			
+			// aapt resource value: 0x7F0700BD
+			public const int xamarin_logo = 2131165373;
 			
 			static Drawable()
 			{
